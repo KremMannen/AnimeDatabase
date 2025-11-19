46f9258ff6a239f4e5477e4e5696ba6b881ac8ef
KremMannen <flyingnorwegians@gmail.com>
2025-11-19 16:47:55 +0100

    lmao

--- app/src/main/java/com/example/eksamensapp/data/api/Anime.kt
+++ app/src/main/java/com/example/eksamensapp/data/api/Anime.kt
@@ -1,12 +1,30 @@
 package com.example.eksamensapp.data.api
 
-data class Anime (
+data class Anime(
     val mal_id: Int,
     val title: String,
-    val imageUrl: String,
+    val images: Images,
     val synopsis: String,
     val score: Double,
     val year: Int,
     val episodes: Int,
-    val genres: List<String>
+    val genres: List<Genre>
+)
+
+data class Images(
+    val jpg: ImageUrl,
+    val webp: ImageUrl?
+)
+
+data class ImageUrl(
+    val image_url: String,
+    val small_image_url: String?,
+    val large_image_url: String?
+)
+
+data class Genre(
+    val mal_id: Int,
+    val type: String,
+    val name: String,
+    val url: String
 )
