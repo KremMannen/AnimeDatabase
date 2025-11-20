34f286513d8d82ad67019da5f3a7759e2b02f37c
mariusaellingsen <marius.a.ellingsen@gmail.com>
2025-11-20 16:24:50 +0100

    small changes to home screen design

--- app/src/main/java/com/example/eksamensapp/screens/home/AnimeHomeScreen.kt
+++ app/src/main/java/com/example/eksamensapp/screens/home/AnimeHomeScreen.kt
@@ -15,12 +15,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -48,10 +50,8 @@ fun AnimeHomeScreen(
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp)
-                .padding(top = 16.dp)
+                .padding(16.dp)
                 .height(150.dp),
-            shape = RoundedCornerShape(16.dp),
             border = BorderStroke(1.dp, LightGrayBorderColor)
         ) {
             Box(
@@ -82,7 +82,12 @@ fun AnimeHomeScreen(
             }
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Divider(
+            color = LightGrayBorderColor, // Eller LightGrayBorderColor
+            thickness = 3.dp,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
         LazyColumn(
             modifier = Modifier.padding(horizontal = 16.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp),
